f3556dd11d5a902ace0548633eca05c8ca15b8fa
Jethnithi <bagpigham@gmail.com>
2018-11-17 22:51:54 +0700

    add, alter fields

--- apis/models.py
+++ apis/models.py
@@ -4,3 +4,35 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+class Category(models.Model):
+    category_id = models.AutoField(max_length=10,primary_key=True)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return "%s" %  (self.name)
+
+class Content(models.Model):
+    content_id = models.AutoField(max_length=10,primary_key=True)
+    text =  models.TextField()
+    url = models.CharField(max_length=255)
+    categories = models.ManyToManyField(Category)
+    created_on = models.DateTimeField(auto_now_add=True) 
+
+class Trend(models.Model):
+    trend_id = models.AutoField(max_length=10,primary_key=True)
+    name = models.CharField(max_length=255)
+    tweet_volume = models.IntegerField(null=True,blank=True,default=0)
+    content = models.ManyToManyField(Content)
+    def __str__(self):
+        return "%s" %  (self.name)
+    
+class Article(models.Model):
+    article_id = models.AutoField(max_length=10,primary_key=True)
+    title = models.CharField(max_length=255)
+    text =  models.TextField()
+    url = models.CharField(max_length=255)
+    categories = models.ManyToManyField(Category)
+    created_on = models.DateTimeField(auto_now_add=True) 
+    def __str__(self):
+        return "%s" %  (self.title)
