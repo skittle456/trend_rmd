56dc557dc04f5ca0fd6000789704630619f9f05b
unknown <devilagri@hotmail.co.th>
2019-06-01 19:19:01 +0700

    Change predict.py

--- model/predict.py
+++ model/predict.py
@@ -24,7 +24,9 @@ MODEL_PATH='../model/'
 DATASET_PATH = f'{MODEL_PATH}dataset_eng9/'
 TESTSET_PATH = f'{MODEL_PATH}testset_eng9/'
 CATEGORIES = ["business","entertainment","food","health","music","plant","politics","sport","tech"]
-model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
+path = os.path.join(DATASET_PATH,"tech/")
+text=open(path+"1.txt","r")
+
 
 import numpy as np
 import tensorflow as tf
@@ -32,23 +34,20 @@ import tensorflow as tf
 from model64_english import cnn_model_fn
 
 tf.logging.set_verbosity(tf.logging.INFO)
-
+model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
 
 def predict(input_string):
-    fullText=input_string
+    fullText=""
     text = input_string
-    #for line in text:
-    #    fullText = str(fullText)+ str(line.decode("utf-8"))
-    print("Fulltext",fullText)
+    for line in text:
+        fullText = str(fullText)+ str(line)
     #clean_text=re.sub("[A-Za-z0123456789!\#\$%\&'\*\+\-\.\^_`\|\~:\(\)\,\\\ ]+",'',fullText)
     clean_text=re.sub("[^A-z ]+",'',fullText)
     result=word_tokenize(clean_text)
     result= [x.lower() for x in result]
-    print("Result",result)
     
     counts = Counter(result)
     counts = words_filter(counts) #Filtering start here !!!!!!!!!!!!!!!!!!!!!!!!!!!
-    print("Counts Filtered",counts)
     ##HERE Pure augment
     words=counts.most_common()
     lst=[]
@@ -84,6 +83,6 @@ def predict(input_string):
     else :
         print("fail")
 
-    
-text="After hours of teasing via a cryptic Twitch stream, today, Hideo Kojima revealed his next game, <b>Death Stranding</b>, will be launching on the PS4 on November 8th. The lengthy stream featured a series of handprints covering what appeared to be gameplay&nbsp;... \n After hours of teasing via a cryptic Twitch stream, today, Hideo Kojima revealed his next game, <b>Death Stranding</b>, will be launching on the PS4 on November 8th. The lengthy stream featured a series of handprints covering what appeared to be gameplay&nbsp;... \n After hours of teasing via a cryptic Twitch stream, today, Hideo Kojima revealed his next game, <b>Death Stranding</b>, will be launching on the PS4 on November 8th. The lengthy stream featured a series of handprints covering what appeared to be gameplay&nbsp;..."
-predict(text)
+
+
+print(predict(text))
